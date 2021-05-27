70567acc82cc9ed52bd31dd3a275eb3e5ecfb2f3
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-27 19:41:57 +0530

    secondcommit

--- filewithpermissions.sh
+++ filewithpermissions.sh
@@ -8,24 +8,3 @@ else
 echo "it is not a file"
 fi
 
-
-if [ -r $1 ]
-then
-echo "read:yes"
-else
-echo "read:no"
-fi
-
-if [ -w $1 ]
-then
-echo "write:yes"
-else
-echo "write:no"
-fi
-
-if [ -x $1 ]
-then
-echo "execute:yes"
-else
-echo "execute:no"
-fi
