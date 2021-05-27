b07b2d72782f0a33f2a63d4e9ae2733e0f45d690
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-27 11:10:29 +0530

    third commit

--- simpleinterest.sh
+++ simpleinterest.sh
@@ -1,11 +1,4 @@
 #! /bin/bash
 
-echo -e "enter the principle: \c"
-read -r $1
-echo -e "enter the time: \c"
-read -r $2
-echo -e "enter the rate: \c"
-read -r $3
-
 simpleinterest=$(((principle*time*rate)/100))
 echo "$simpleinterest"
