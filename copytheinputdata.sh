f19935bd0d882a033c1abfb6f9ec198e0d8f95a8
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-27 20:36:05 +0530

    Create copytheinputdata.sh

---
+++ copytheinputdata.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+echo "cd no.: $cdno" >> filex.txt
+
+
+echo "moviename: $moviename" >> filex.txt
+
+
+echo "language: $language" >> filex.txt
+
+
+echo "price: $price" >> filex.txt
+
+
+echo "date of release: $date_of_release" >> filex.txt
+
