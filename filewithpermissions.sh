0b5cc3df0efafd90b7c8c9cea2daefa7d27d4d15
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-27 19:14:17 +0530

    firstcommit

---
+++ filewithpermissions.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+echo -e "enter the filename: \c"
+read -r filename
+
+if [ -f $filename ]
+then
+echo "it is a file: $filename"
+else 
+echo "it is not a file"
+fi
+
+
+if [ -r $filename ]
+then
+echo "read:yes"
+else
+echo "read:no"
+fi
+
+if [ -w $filename ]
+then
+echo "write:yes"
+else
+echo "write:no"
+fi
+
+if [ -x $filename ]
+then
+echo "execute:yes"
+else
+echo "execute:no"
+fi
