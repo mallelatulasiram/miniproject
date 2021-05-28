25f534effc5c398ffee7124829bc72b4b9cf2b1a
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-28 15:16:14 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -10,22 +10,22 @@ echo "4: division"
 echo "======================================="
 
 
-if [ $what_operation == 1 ];then
+if [ $what_operation == addition ];then
 
 addition=`expr $a + $b`
 echo 1: addition: $addition
 
-elif [ $what_operation == 2 ];then
+elif [ $what_operation == subration ];then
 
 subration=`expr $a - $b`
 echo 2: subration: $subration
 
-elif [ $what_operation == 3 ];then
+elif [ $what_operation == multiplication ];then
 
 multiplication=`expr $a \* $b`
 echo 3: multiplication: $multiplication
 
-elif [ $what_operation == 4 ];then
+elif [ $what_operation == division ];then
 
 division=`expr $a / $b`
 echo 4: division: $division
