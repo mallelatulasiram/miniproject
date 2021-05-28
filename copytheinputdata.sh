21c16841aae57457cbb5e406484baf9ccb4b2000
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-05-28 07:55:39 +0530

    Update copytheinputdata.sh

--- copytheinputdata.sh
+++ copytheinputdata.sh
@@ -1,24 +1,19 @@
 #! /bin/bash
 
-echo -n "enter cd no.: "
-read cdno
-echo "cd no.: $cdno" >> filex.txt
 
-echo -n "enter moviename: "
-read moviename
-echo "moviename: $moviename" >> filex.txt
+echo "cd no.: $cdno" >> /home/ubuntu/filex.txt
 
-echo -n "enter language: "
-read language
-echo "language: $language" >> filex.txt
 
-echo -n "enter price: "
-read price
-echo "price: $price" >> filex.txt
+echo "moviename: $moviename" >> /home/ubuntu/filex.txt
 
-echo -n "enter date of release: "
-read date_of_release
-echo "date of release: $date_of_release" >> filex.txt
+
+echo "language: $language" >> /home/ubuntu/filex.txt
+
+
+echo "price: $price" >> /home/ubuntu/filex.txt
+
+
+echo "date of release: $date_of_release" >> /home/ubuntu/filex.txt
 
 
 
